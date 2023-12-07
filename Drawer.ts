5054ca2ddfa11262721f882ab3dc43daa32c708b
Ted Avtar <theted@berkeley.edu>
2023-12-07 15:31:20 -0800

    Add gridlines to Drawer

--- Drawer.ts
+++ Drawer.ts
@@ -4,8 +4,9 @@ export class Drawer {
         canvas: HTMLCanvasElement;
         context: CanvasRenderingContext2D;
 	gameState: GameState;
-	width: number = 700;
-	height: number = 600;
+	scaleFactor: number = 100;
+	width: number = GameState.numCols * this.scaleFactor;
+	height: number = GameState.numRows * this.scaleFactor;
 
 	constructor(canvas: HTMLCanvasElement, gameState: GameState){
 		this.canvas = canvas;
@@ -21,6 +22,22 @@ export class Drawer {
 		this.context.fillRect(0, 0, this.width, this.height);
 
 		// draw gridlines
+		for(let i=0; i<=GameState.numCols; i++) {
+			this.context.lineWidth = 5;
+			this.context.beginPath();
+			this.context.moveTo(this.scaleFactor * i, 0);
+			this.context.lineTo(this.scaleFactor * i, this.height);
+			this.context.strokeStyle = "brown";
+			this.context.stroke();
+		}
+		for(let i=0; i<=GameState.numRows; i++) {
+			this.context.lineWidth = 5;
+			this.context.beginPath();
+			this.context.moveTo(0, this.scaleFactor * i);
+			this.context.lineTo(this.width, this.scaleFactor * i);
+			this.context.strokeStyle = "brown";
+			this.context.stroke();
+		}
 
 
 		// draw the non-vacant cells from gameState
