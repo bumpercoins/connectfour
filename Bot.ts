d8de0d23ebb4538b4edba63b859f94082003bd7f
Ted Avtar <theted@berkeley.edu>
2024-03-06 16:36:44 -0800

    Use MCTS improved policy in game, not simply vanilla net

--- Bot.ts
+++ Bot.ts
@@ -2,7 +2,7 @@ import { GameState } from "./GameState";
 import { Policy } from "./Policy";
 import { Move } from "./Move";
 import * as tf from '@tensorflow/tfjs';
-import { getPolicyAndValue } from "./Net";
+import { doMCTS } from "./MCTS";
 
 export class Bot {
 	isP1Bot: boolean;
@@ -14,7 +14,8 @@ export class Bot {
         }
 	
 	getMove(gameState: GameState): Move {
-		let policy: Policy =  getPolicyAndValue(gameState, this.model)[0];
+		//let policy: Policy =  getPolicyAndValue(gameState, this.model)[0];
+		let policy: Policy = doMCTS(gameState, this.model);
 
 		//let dummyBotMove: Move = new Move(this.isP1Bot, Math.floor(Math.random() * GameState.numCols));
 		//return dummyBotMove;
